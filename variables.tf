485bcee6dfbb0f46cf2e1037067086b2b670dcd6
Alex, Lee <cine0831@gmail.com>
2020-07-13 20:36:32 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -52,17 +52,7 @@ variable "igw_tags" {
   default     = {}
 }
 
-variable "public_subnets" {
-  description = "A list of public subnets inside the VPC"
-  type        = list(string)
-  default     = []
-}
 
-variable "private_subnets" {
-  description = "A list of private subnets inside the VPC"
-  type        = list(string)  
-  default     = []
-}
 
 variable "tags" {
   description = "A map of tags to add to all resources"
