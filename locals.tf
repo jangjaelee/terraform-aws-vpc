7faedf9784e495d495ff53637a77aca552227563
Alex Lee <cine0831@gmail.com>
2021-10-27 10:56:16 +0900

    update Cloud Platform

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
   default_nacl_id = var.create_vpc ? element(concat(aws_vpc.this.*.default_network_acl_id, [""]), 0) : var.vpc_id
 
   common_tags = {
-    "cloud/platform" = "KubeSphere"
+    "cloud/platform" = "AWS"
     "cloud/platform_env"  = var.env
     "cloud/managed_by" = "terraform"
     "cloud/module" = "vpc"
